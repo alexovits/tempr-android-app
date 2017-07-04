7ffb6420ecbb1fd0c64d7f886c623ad2f224e3b4
alexovits <debeloksanyi@gmail.com>
2017-07-04 23:39:51 +0300

    Create signup view

---
+++ app/src/main/java/edu/ubb/tempr/ui/signup/SignUpActivity.java
@@ -0,0 +1,15 @@
+package edu.ubb.tempr.ui.signup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.ubb.tempr.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
